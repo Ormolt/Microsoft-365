75eca2e445f78b88aea0c7436372426924a0d3a2
Ormolt <ormolt88@gmail.com>
2023-12-31 06:44:03 -0500

    Create Microsoft.py

---
+++ Microsoft.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+from random import randint
+
+for i in range(1, 365):
+
+    for j in range(0, randint(1, 10)):
+        d = str(i) + ' days ago'
+        with open('file.txt', 'a') as file:
+            file.write(d)
+        os.system('git add .')
+        os.system('git commit --date="' + d + '"-m "commit"')
+            
+os.system('git push -u origin main')
+
+
+# In[ ]:
+
+
+
+
